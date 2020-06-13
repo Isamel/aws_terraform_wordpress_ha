f6bbf8a32ce44545dba4ebb8a2fb539183e6412a
unknown <Jose Salinas>
2020-06-13 14:44:42 -0700

    change undeclared variables

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "route53_zone_terraform" {
     source = "git@github.com:Isamel/aws_terraform_route53_zone.git"
     
-    route53_zone_count      = var.route53_zone_count
+    route53_zone_count      = var.enabled
     route53_zone_depends_on = var.route53_zone_depends_on
     route53_zone_name       = var.route53_zone_name
     extra_tags              = local.tags
@@ -10,7 +10,7 @@ module "route53_zone_terraform" {
 module "security_group_terraform" {
     source = "git@github.com:Isamel/aws_terraform_route53_zone.git"
     
-    security_group_count      = var.security_group_count
+    security_group_count      = var.enabled
     security_group_depends_on = var.security_group_depends_on
     security_group_name       = var.security_group_name
     security_group_vpc_id     = var.security_group_vpc_id
