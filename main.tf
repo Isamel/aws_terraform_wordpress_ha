cdf8e24032d8592bdc73816daed942742fed1ad4
unknown <Jose Salinas>
2020-06-13 14:41:41 -0700

    add security group

--- main.tf
+++ main.tf
@@ -6,3 +6,15 @@ module "route53_zone_terraform" {
     route53_zone_name       = var.route53_zone_name
     extra_tags              = local.tags
 }
+
+module "security_group_terraform" {
+    source = "git@github.com:Isamel/aws_terraform_route53_zone.git"
+    
+    security_group_count      = var.security_group_count
+    security_group_depends_on = var.security_group_depends_on
+    security_group_name       = var.security_group_name
+    security_group_vpc_id     = var.security_group_vpc_id
+    security_group_ingress    = var.security_group_ingress
+    security_group_egress     = var.security_group_egress
+    extra_tags                = local.tags
+}
--- variables.tf
+++ variables.tf
@@ -20,6 +20,31 @@ variable "route53_zone_name" {
     description = "route53 zone name."
 }
 
+variable "security_group_depends_on" {
+    type        = list(string)
+    description = "security group depends on."
+}
+
+variable "security_group_name" {
+    type        = string
+    description = "security group name."
+}
+
+variable "security_group_vpc_id" {
+    type        = string
+    description = "security group vpc id."
+}
+
+variable "security_group_ingress" {
+    type        = list(map(string))
+    description = "security group ingress."
+}
+
+variable "security_group_egress" {
+    type        = list(map(string))
+    description = "security group egress."
+}
+
 variable "extra_tags" {
     type        = map(string)
     description = "extra tags."
