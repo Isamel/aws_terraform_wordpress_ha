fba1c673cac20deb21d9864a492d31498197b9c9
unknown <Jose Salinas>
2020-06-13 18:08:53 -0700

    add application load balancer

--- main.tf
+++ main.tf
@@ -18,3 +18,26 @@ module "security_group_terraform" {
     security_group_egress     = var.security_group_egress
     extra_tags                = local.tags
 }
+
+data "aws_subnet_ids" "subnet_ids_terraform" {
+    vpc_id = var.vpc_id
+}
+
+module "alb_terraform" {
+    source = "git@github.com:Isamel/aws_terraform_application_load_balancer.git"
+    
+    alb_count                                 = var.enabled ? 1 : 0
+    alb_depends_on                            = [
+        join("", module.route53_zone_terraform.route53_zone.*.zone_id),
+        join("", module.security_group_terraform.security_group.*.id)
+    ]
+    alb_name                                  = var.alb_name
+    alb_internal                              = var.alb_internal
+    alb_idle_timeout                          = var.alb_idle_timeout
+    alb_idle_load_balancer_type               = var.alb_idle_load_balancer_type
+    alb_idle_security_groups                  = [join("", module.security_group_terraform.security_group.*.id)]
+    alb_idle_subnets                          = [data.aws_subnet_ids.subnet_ids_terraform[0], data.aws_subnet_ids.subnet_ids_terraform[1],]
+    alb_idle_enable_deletion_protection       = var.alb_idle_enable_deletion_protection
+    alb_idle_enable_cross_zone_load_balancing = var.alb_idle_enable_cross_zone_load_balancing
+    extra_tags                                = local.tags
+}
--- variables.tf
+++ variables.tf
@@ -45,6 +45,41 @@ variable "security_group_egress" {
     description = "security group egress."
 }
 
+variable "alb_name" {
+    type        = string
+    description = "alb name."
+}
+
+variable "alb_internal" {
+    type        = bool
+    description = "alb internal."
+}
+
+variable "alb_idle_timeout" {
+    type        = number
+    description = "alb idle timeout."
+}
+
+variable "alb_idle_load_balancer_type" {
+    type        = string
+    description = "alb idle load balancer type."
+}
+
+variable "alb_idle_security_groups" {
+    type        = list(string)
+    description = "alb idle security groups."
+}
+
+variable "alb_idle_enable_deletion_protection" {
+    type        = bool
+    description = "alb idle enable deletion protection."
+}
+
+variable "alb_idle_enable_cross_zone_load_balancing" {
+    type        = bool
+    description = "alb idle enable cross zone load balancing."
+}
+
 variable "extra_tags" {
     type        = map(string)
     description = "extra tags."
